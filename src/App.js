c8920f8b42ab07540c9844dcc1e90702e26cfe7b
Thomas Gerdes <16652512+thomasgerdes@users.noreply.github.com>
2025-06-27 20:18:10 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RVKClassifier from './components/RVKClassifier';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RVKClassifier />
+    </div>
+  );
+}
+
+export default App;
